080d8cd02c3a571e6a7b30cc1df8ffe070fee2c7
lispalding <lisette.spalding@gmail.com>
2021-04-25 13:01:00 -0600

    04/25/2021 Commit -- 1:00 PM

--- settings.py
+++ settings.py
@@ -1,47 +1,70 @@
 # MADE BY: Lisette Spalding
+# ART WORK CREDIT: "Kenney.nl" @ "www.kenney.nl"
 # FILE NAME: settings.py
 # PROJECT NAME: python__simple_platformer_game
 # DATE CREATED: 04/01/2021
-# DATE LAST MODIFIED: 04/24/2021
+# DATE LAST MODIFIED: 04/25/2021
 # PYTHON VER. USED: 3.8
 
 ################### IMPORTS ####################
 import pygame as pg
 import random as r
-from os import *
+from os import path
 ################### FINISHED ###################
 
-########## VARIABLES ###########
+################## VARIABLES ###################
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
+
+FONT_NAME = "arial"
 
 # Player Properties
 PLAYER_ACCELERATION = 0.5
 PLAYER_FRICTION = -0.12
 
-PLAYER_WIDTH = 50
+PLAYER_GRAVITY = 0.8
+
+PLAYER_JUMP = 20
+
+PLAYER_WIDTH = 25
 PLAYER_HEIGHT = 50
 
 # Title
-title = "Python Platformer"
-########### FINISHED ###########
+TITLE = "Jumpy Platformer - Python"
+################### FINISHED ###################
 
-######### FOLDER SETUP #########
+################## PLATFORMS ###################
+# Starting Platforms:
+PLATFORM_LIST = [(0, HEIGHT - 60),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4),
+                 (350, 200),
+                 (175, 100)]
+
+################### FINISHED ###################
+
+################# FOLDER SETUP #################
 gameFolder = path.dirname(__file__) # General folder set-up
 
 # Basic image folders
 imageFolder = path.join(gameFolder, "images")
-playerImages = path.join(imageFolder, "player_image")
+spritesheetImageFolder = path.join(imageFolder, "Spritesheets")
 
 # Basic sound folder
 soundFolder = path.join(gameFolder, "sounds")
 
 # Basic text data folder
 textDataFolder = path.join(gameFolder, "text_data")
-########### FINISHED ###########
 
-###### COLORS  (R. G. B) ######
+############ FOLDER CONSTANTS ############
+HIGHSCORE_FILE = "highscore.txt"
+SPRITESHEET_FILE = "spritesheet_jumper.png"
+########## FOLDER CONSTANTS FIN ##########
+################### FINISHED ###################
+
+############### COLORS  (R. G. B) ##############
+BGCOLOR = (0, 155, 155) # Background Color
+
 BLACK = (0,0,0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -56,4 +79,4 @@ PURPLE = (182, 103, 224)
 PINK = (224, 103, 139)
 LIGHT_BLUE = (100, 162, 209)
 YELLOW = (245, 233, 154)
-############# FIN #############
+################### FINISHED ###################
